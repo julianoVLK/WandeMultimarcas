d345a8d29da9dceb1b75f49c81a382ad7b158b35
Juliano Volski <juliano_volski@hotmail.com>
2021-11-04 10:41:26 -0300

    Router

---
+++ app/Http/Response.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http;
+
+class Response {
+    private $httpCode = 200;
+    private $content;
+
+    public function __construct($httpCode, $content, $contentType = 'text/html'){
+        $this->httpCode = $httpCode;
+        $this->content = $content;
+        $this->setContentType($contentType);
+    }
+
+    public function setContentType($contentType){
+        $this->contentType = $contentType;
+        $this->addHeader('Content-type', $contentType);
+    }
+
+    public function addHeader($key, $value){
+        $this->headers[$key] = $value;
+    }
+
+    private function sendHeaders(){
+        http_response_code($this->httpCode);
+
+        foreach($this->headers as $key=>$value){
+            header($key.': '.$value);
+        }
+    }
+
+    public function sendResponse(){
+        $this->sendHeaders();
+        switch($this->contentType) {
+            case 'text/html':
+                echo $this->content;
+                exit;
+        }
+    }
+}
--- index.php
+++ index.php
@@ -2,6 +2,18 @@
 
 require __DIR__.'/vendor/autoload.php';
 
+use \App\Http\Router;
+use \App\Http\Response;
 use \App\Controller\Pages\Home;
 
-echo Home::getHome();
+
+define('URL', 'http://localhost/wande');
+$obRouter = new Router(URL);
+
+$obRouter->get('/', [
+    function(){
+        return new Response(200, Home::getHome());
+    }
+]);
+
+$obRouter->run()->sendResponse();
